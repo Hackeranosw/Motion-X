39a49e7b7eb0c3bd69c7f4b4a40a21cbcd475785
Hackeranosw <111694730+Hackeranosw@users.noreply.github.com>
2026-09-19 09:56:54 +0000

    feat: inisialisasi project dan push ke actions

---
+++ motionx/lib/main.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+import 'editor_screen.dart';
+
+void main() {
+  runApp(const MaterialApp(
+    debugShowCheckedModeBanner: false,
+    home: MotionXStudio(),
+  ));
+}
